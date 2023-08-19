f56ebd734aa7ac38a018f2eefeb5180955958716
unknown <shanitakalamkar11@gmail.com>
2023-08-19 17:31:43 +0530

    code pushed

--- src/app/websitedata/websitedata-routing.module.ts
+++ src/app/websitedata/websitedata-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WebsitedataComponent } from './websitedata/websitedata.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'WebsiteData', pathMatch: 'full' },
   {
     path: 'WebsiteData', component: WebsitedataComponent,
     children:[
+      { path: '', redirectTo: 'Home', pathMatch: 'full' },
       {
         path:'Home',component:HomeComponent
+      },
+      {
+        path:'About',component:AboutComponent
       }
     ]
   }
--- src/app/websitedata/websitedata.module.ts
+++ src/app/websitedata/websitedata.module.ts
@@ -6,6 +6,8 @@ import { WebsitedataComponent } from './websitedata/websitedata.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { FooterComponent } from './footer/footer.component';
     WebsitedataComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    GalleryComponent
   ],
   imports: [
     CommonModule,
